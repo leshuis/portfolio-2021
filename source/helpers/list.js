b8a6c8b1776b95e017ae5caf9a7f8c775c66ef14
Leonard Eshuis <leonardeshuis@gmail.com>
2021-05-31 23:36:27 +0200

    update
    update

--- source/helpers/list.js
+++ source/helpers/list.js
@@ -1,23 +1,24 @@
 var helpers = module.exports;
 
-helpers.list = function (dataItems) {
+helpers.list = function (dataItems, title = "") {
     const itemType = Object.keys(dataItems)[0];
     const items = dataItems[itemType];
     let item = null;
 
-    var out = "<ul class='list list__clean'>";
+    var out = `<ul class='list list__clean'><h3 class="list__header">${title}</h3>`;
 
     for (let i = 0, l = items.length; i < l; i++) {
         item = items[i];
         out += '<li class="list_clean">';
-        if (itemType == 'links') {
-            out += "<a href='" + item.url + "'>" + item.text + "</a>";
-        }
-        if (['skill','experience','education','course'].indexOf(itemType) >= 0) {
+        if (['social', 'skill', 'experience', 'education', 'course'].indexOf(itemType) >= 0) {
 
             out += `<ul class="${itemType} list__clean">`;
             for (const [key, value] of Object.entries(item)) {
-                out += `<li class="${itemType}__${key}">${value}</li>`;
+                if (key == 'link') {
+                    out += "<li><a href='" + value.url + "'>" + value.text + "</a></li>";
+                } else {
+                    out += `<li class="${itemType}__${key}">${value}</li>`;
+                }
             }
             out += '</ul>'
         }
